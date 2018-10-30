260abe02ffb99cef4bf2e601b46e4d54a7ec1bfd
Elie <elie.bironneau@capgemini.com>
2018-10-30 17:09:18 +0100

    Update des librairies

--- penaliche/src/main/java/penaliche/penaliche/rsl/ServicePhrases.java
+++ penaliche/src/main/java/penaliche/penaliche/rsl/ServicePhrases.java
@@ -1,10 +1,23 @@
 package penaliche.penaliche.rsl;
 
 import java.util.List;
-import penaliche.penaliche.dto.Phrase;
 
+import penaliche.penaliche.dto.Phrase;
+import penaliche.penaliche.dto.RequetePhrase;
+
+/**
+ * Web services exposés pour le traitememnt de phrase.
+ * @author ebironne
+ *
+ */
 public interface ServicePhrases {
-    public String soumettrePhrase(Phrase var1);
+	
+	/**
+	 * Permet de faire une demande d'ajout de phrase.
+	 * @param reqPhrase la requete.
+	 * @return une chaine
+	 */
+    public String soumettrePhrase(RequetePhrase reqPhrase);
 
     public List<Phrase> recupererPhrase();
 }
